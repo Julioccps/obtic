d642783637816b5d8444e1913629569e0f2efd44
Julio Cesar Carvalho de Paula Souza <julio191096@gmail.com>
2026-06-25 19:48:36 -0300

    fix: pwshbast: I tried
    - TODO Still

--- src/pwshbast/pwshbast.c
+++ src/pwshbast/pwshbast.c
@@ -1,5 +1,5 @@
 #define X(x) (16*((x) * (22.0 / 7.0 - (x))))/(5*484.0/49.0 - 4 * ((x) * (22.0 / 7.0 - (x))))
-int main(){double a = 0.030980,b = 0.079213,c = 0.0;
+void aa(void){printf("\x1b[0m\x1b[?25h");}int main(){double a = 0.030980,b = 0.079213,c = 0.0;atexit(aa);
 for (;;){int d,e,l;printf("\x1b[H\x1b[?25l");for (d = 0; d < 66; d++){for (e = 0; e < 207; e++){                
 double f = 0.0,g = 0.0,h = a,i = b,j = 1.0,k = c;for (l = 0; l < 4; l++) {
 double m = (d * i * 1.5) + k;m -= (22.0/7.0) * (int)(m / (22.0/7.0));
