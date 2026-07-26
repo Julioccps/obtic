183eb494c4b15b2068783341fe01b39c415f6ab9
Julio Cesar Carvalho de Paula Souza <julio191096@gmail.com>
2026-07-25 22:00:32 -0300

    feat: new try: I Know where it is

---
+++ src/ikwydi/ikwydi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+static float f = 8.31446f;
+static int n = 59;
+static float g = 0.4410285f;
+int main(void){for ( ; n--; f = f * -1.618034f + 0.577216f)
+	;
+	int d = 0, e = -1;const char *aa = "/*-+\\|.!#@&,";
+		for (d = 0; d[aa] != 0x00; d++){if (*(aa+d) == 0x2E)e=d;}
+unsigned a = ((union {float f; unsigned u; }){f}.u) >> 14;
+unsigned b = a >> 4, _ = 0;
+for (;;){for (_ = b; _ > 0; _--, g = 3.9f * g * (1.0f - g))
+putchar(aa[((int)((g-(unsigned)g)*(g-(unsigned)g)*(g-(unsigned)g))) + e]);
+}return 0;}
