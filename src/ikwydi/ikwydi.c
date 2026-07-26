6a724c4a660065823c1516447d2895fc0c98f585
Julio Cesar Carvalho de Paula Souza <julio191096@gmail.com>
2026-07-25 22:09:20 -0300

    fix: ikwydi: yeah, no infinity around here partner

--- src/ikwydi/ikwydi.c
+++ src/ikwydi/ikwydi.c
@@ -9,6 +9,6 @@ int main(void){for ( ; n--; f = f * -1.618034f + 0.577216f)
 		for (d = 0; d[aa] != 0x00; d++){if (*(aa+d) == 0x2E)e=d;}
 unsigned a = ((union {float f; unsigned u; }){f}.u) >> 14;
 unsigned b = a >> 4, _ = 0;
-for (;;){for (_ = b; _ > 0; _--, g = 3.9f * g * (1.0f - g))
+for (_ = b; _ > 0; _--, g = 3.9f * g * (1.0f - g))
 putchar(aa[((int)((g-(unsigned)g)*(g-(unsigned)g)*(g-(unsigned)g))) + e]);
-}return 0;}
+	return 0;}
